92cf46c5d6a127b38c1f526a8405b2d8d4adb9e6
Unguliant <ungulient@gmail.com>
2016-08-03 18:00:13 +0300

    Hello Alon.

--- app/src/main/java/com/project/shechter/projectshechter/MainActivity.java
+++ app/src/main/java/com/project/shechter/projectshechter/MainActivity.java
@@ -3,13 +3,18 @@ package com.project.shechter.projectshechter;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothSocket;
 import android.content.Intent;
+import android.os.Handler;
+import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TabHost;
 import android.widget.Toast;
 
 
@@ -21,33 +26,62 @@ import java.util.UUID;
 import com.jmedeisis.bugstick.Joystick;
 import com.jmedeisis.bugstick.JoystickListener;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    Button button_tlat_mode;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    //TODO IDO REMOVE ALL DEBUGS
+    private static final boolean DEBUG = true;
+    //TODO IDO REMOVE ALL DEBUGS
+
+    //Widgets ==================
     Joystick joystick;
     WebView camera;
-    static final String camera_url = "https://www.google.com/"; //TODO change url
-    String bluetooth_string;
-    byte[] joystick_origin;
+    TabHost tab_host_controls;
+    ImageView tlatControlsTemp; //TODO IDO DEFINE HERE
+    //==========================
+
+
+    //Camera & Shech's address ====================================================
+    //TODO IDO ad hoc? perhaps let user choose?
+    static final String CAMERA_URL = "https://www.google.com/";
+    static final String SHECH_MAC = "98:D3:31:F4:06:76";
+    //=============================================================================
+
+
+    //Joystick ================================================================
+    static final byte[] JOYSTICK_ORIGIN = ("H" + String.format("%+04d", 0)
+            +   "V" + String.format("%+04d", 0)
+            +   "E" + Character.toString(checkNum(0))).getBytes();;
     int joystick_x,
         joystick_y;
+    long previous_joystick_tick = SystemClock.elapsedRealtime();
+    boolean joystick_busy = false;
+    static final long JOYSTICK_INTERVAL = 10;
+    Runnable write_joystick_origins = new Runnable() {
+        @Override
+        public void run() {
+            joystick_x = 0;
+            joystick_y = 0;
 
-    //TODO temp
-    //ImageView tlatControlsTemp;
-    final static String SHECH_MAC = "98:D3:31:F4:06:76";
-    //TODO temp
+            if (DEBUG) {
+                Log.d("Joystick", new String(JOYSTICK_ORIGIN));
+            } else {
+                bluetooth_thread.write(JOYSTICK_ORIGIN);
+            }
 
-    enum Mode{
-        DRIVE,TLAT
-    }
-    Mode mode;
+            previous_joystick_tick = SystemClock.elapsedRealtime();
+            joystick_busy = false;
+        }
+    };
+    Handler handler = new Handler();
+    //=========================================================================
 
+
+    //Bluetooth ===========================
     BluetoothAdapter m_bluetooth_adapter;
     ConnectedThread bluetooth_thread;
-
-    //TODO IDO EDIT
-    //Set<BluetoothDevice> pairedDevices;
-    //ArrayAdapter<String> mArrayAdapter;
-
+    //=====================================
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -59,69 +93,95 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
         //TODO IDO EDIT BETTER SOLUTION
 
-        bluetooth_string = new String();
-
-        //init button
+       /* //init button
         button_tlat_mode = (Button) findViewById(R.id.buttonTlatMode);
         //define button listeners
-        button_tlat_mode.setOnClickListener(this);
+        button_tlat_mode.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //change between drive/tlat modes
+                if (mode == Mode.DRIVE) {
+                    button_tlat_mode.setText(R.string.driving_mode);
+                    mode = Mode.TLAT;
+                } else {
+                    button_tlat_mode.setText(R.string.tlat_mode);
+                    mode = Mode.DRIVE;
+                }
+            }
+        });*/
 
+        // init camera
+        camera = (WebView) findViewById(R.id.camera);
+        camera.loadUrl(CAMERA_URL);
+        camera.setWebViewClient(new WebViewClient());
+
+        //init controls
+        tab_host_controls = (TabHost) findViewById(R.id.tabHostControls);
         //init joystick
-        joystick_origin = ("H" + String.format("%+04d", 0)
-                +   "V" + String.format("%+04d", 0)
-                +   "E" + Character.toString(checkNum(0))).getBytes();
         joystick = (Joystick) findViewById(R.id.joystick);
         joystick.setJoystickListener(new JoystickListener() {
             @Override
             public void onDown() {
-                bluetooth_thread.write(joystick_origin);
 
-                //TODO IDO EDIT PERHAPS WITH DELAY??
-                bluetooth_thread.write(joystick_origin);
             }
 
             @Override
             public void onDrag(float degrees, float offset) {
-                joystick_x = (int) Math.round(Math.cos(degrees * Math.PI / 180f) * offset * 100f);
-                joystick_y = (int) Math.round(Math.sin(degrees * Math.PI / 180f) * offset * 100f);
+                if ( joystick_busy ||
+                        SystemClock.elapsedRealtime() - previous_joystick_tick < JOYSTICK_INTERVAL) {
+                    return;
+                }
 
-                bluetooth_string =  "H" + String.format("%+04d", joystick_x)
+                int new_joystick_x = (int) Math.round(Math.cos(degrees * Math.PI / 180f) * offset * 100f),
+                        new_joystick_y = (int) Math.round(Math.sin(degrees * Math.PI / 180f) * offset * 100f);
+
+                if (new_joystick_x == joystick_x && new_joystick_y == joystick_y) {
+                    return;
+                }
+
+                joystick_x = new_joystick_x;
+                joystick_y = new_joystick_y;
+
+                String bluetooth_string =  "H" + String.format("%+04d", joystick_x)
                         +   "V" + String.format("%+04d", joystick_y)
                         +   "E" + Character.toString(checkNum(joystick_x + joystick_y));
 
-                bluetooth_thread.write(bluetooth_string.getBytes());
+                if (DEBUG) {
+                    Log.d("Joystick",bluetooth_string);
+                } else{
+                    bluetooth_thread.write(bluetooth_string.getBytes());
+                }
+
+                previous_joystick_tick = SystemClock.elapsedRealtime();
             }
 
             @Override
             public void onUp() {
-                bluetooth_thread.write(joystick_origin);
+                if(joystick_busy || (joystick_x == 0 && joystick_y == 0)) {
+                    return;
+                }
+                joystick_busy = true;
+                long time_left = JOYSTICK_INTERVAL - SystemClock.elapsedRealtime() + previous_joystick_tick;
 
-                //TODO IDO EDIT PERHAPS WITH DELAY??
-                bluetooth_thread.write(joystick_origin);
+                if (time_left <= 0 ){
+                    write_joystick_origins.run();
+                } else {
+                    handler.postDelayed(write_joystick_origins, time_left);
+                }
             }
         });
-
-        // init camera
-        camera = (WebView) findViewById(R.id.camera);
-        camera.loadUrl(camera_url);
-        camera.setWebViewClient(new WebViewClient());
-
         //init tlat controls
-        /*tlatControlsTemp = (ImageView) findViewById(R.id.tlatControlsTemp); //TODO temp
-        tlatControlsTemp.setVisibility(View.GONE);*/
+        tlatControlsTemp = (ImageView) findViewById(R.id.tlatControlsTemp); //TODO temp
 
-        // mode
-        mode = Mode.DRIVE;
 
         //Bluetooth
         m_bluetooth_adapter = BluetoothAdapter.getDefaultAdapter();
         // Check whether BT is supported
         if (m_bluetooth_adapter == null) {
-            Toast.makeText(getApplicationContext(), "No Bluetooth on this device.", Toast.LENGTH_SHORT).show();
+            Toast.makeText(getApplicationContext(), "No Bluetooth on this device.", Toast.LENGTH_LONG).show();
             finish();
         }
     }
-
 
     @Override
     protected void onResume() {
@@ -134,89 +194,31 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             startActivityForResult(enableBtIntent, 0);
         }
 
-
-        try {
-            bluetooth_thread = new ConnectedThread(
-                    m_bluetooth_adapter.getRemoteDevice(SHECH_MAC).createRfcommSocketToServiceRecord(
-                            UUID.fromString("00001101-0000-1000-8000-00805F9B34FB")
-                    )
-            );
-        } catch (IOException e) {
-            //TODO IDO EDIT
-            Toast.makeText(getApplicationContext(), "LOLS IDO LOLOLS", Toast.LENGTH_SHORT).show();
-            finish();
-            //TODO IDO EDIT
-        }
-
-        try {
-            bluetooth_thread.connect();
-        } catch (IOException e) {
-            //TODO IDO EDIT
-            Toast.makeText(getApplicationContext(), "Can't connect", Toast.LENGTH_SHORT).show();
-            finish();
-            //TODO IDO EDIT
-        }
-
-
-        /*
-        pairedDevices = m_bluetooth_adapter.getBondedDevices();
-        mArrayAdapter = new ArrayAdapter<String>();
-        // If there are paired devices
-        if (pairedDevices.size() > 0) {
-            // Loop through paired devices
-            for (BluetoothDevice device : pairedDevices) {
-                // Add the name and address to an array adapter to show in a ListView
-                mArrayAdapter.add(device.getName() + "\n" + device.getAddress());
+        if(!DEBUG) {
+            try {
+                bluetooth_thread = new ConnectedThread(
+                        m_bluetooth_adapter.getRemoteDevice(SHECH_MAC).createRfcommSocketToServiceRecord(
+                                UUID.fromString("00001101-0000-1000-8000-00805F9B34FB")
+                        )
+                );
+                bluetooth_thread.connect();
+            } catch (IOException e) {
+                //TODO IDO EDIT
+                Toast.makeText(getApplicationContext(), "Can't connect", Toast.LENGTH_LONG).show();
+                finish();
+                //TODO IDO EDIT
             }
-        }*/
-
-        /*
-        //TODO IDO ENABLE NOT USING ADMIN, NEED TO FIX
-        Integer resultCode = new Integer(0);
-        while (!m_bluetooth_adapter.isEnabled()) {
-            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
-            onActivityResult(REQUEST_ENABLE_BT, resultCode, enableBtIntent);
-            if(resultCode == RESULT_OK ) {
-                break;
-            }
-        }
-        */
-
-
-    }
-
-    @Override
-    public void onClick(View v) {
-        //for all buttons
-        switch (v.getId()) {
-            case R.id.buttonTlatMode:
-                //change between drive/tlat modes
-                if (mode == Mode.DRIVE) {
-                    button_tlat_mode.setText(R.string.drive);
-                    mode = Mode.TLAT;
-                    joystick.setVisibility(View.GONE);
-                    //tlatControlsTemp.setVisibility(View.VISIBLE);
-                } else {
-                    button_tlat_mode.setText(R.string.t_l_a_t_mode);
-                    mode = Mode.DRIVE;
-                    joystick.setVisibility(View.VISIBLE);
-                    //tlatControlsTemp.setVisibility(View.INVISIBLE);
-                }
-                break;
-            default:
-
         }
     }
 
-    private char checkNum(int i) {
+    static private char checkNum(int i) {
         int a = i / 100;
         int c = i % 100;
         int b = c / 10;
         c = c % 10;
         int x = a + b + c;
 
-        while (x>9){
+        while (x > 9){
             x = x / 10 + x % 10;
         }
 
